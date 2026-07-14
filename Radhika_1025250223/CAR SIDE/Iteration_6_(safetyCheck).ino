f476a79d09306319e022b78e6097038e2562d844
Radhika--88 <radhikamangla422@gmail.com>
2026-07-14 13:20:33 +0530

    Remove Radhika_1025250223

--- Radhika_1025250223/CAR SIDE/Iteration_6_(safetyCheck).ino
+++
@@ -1,7 +0,0 @@
-// if no packet is recieved till the 300 miliseconds the car will stop automatically
-void safetyCheck() {
-  if (millis() - lastPacketTime > 300) {
-    receivedCmd.direction = 0;
-    receivedCmd.speed = 0;
-  }
-}
